6938edff374ff2228deefbfc43a1a6fa4c6fd676
sy <1766342177@qq.com>
2021-03-29 20:54:35 +0800

    pull更新了readme，push检查一下

---
+++ src/main/java/com/testing/class16/InterDDTest.java
@@ -0,0 +1,13 @@
+package com.testing.class16;
+
+/**
+ * @Classname InterDDTest
+ * @Description 类型说明
+ * @Date 2021/3/29 20:27
+ * @Created by 测试园
+ */
+public class InterDDTest {
+    public static void main(String[] args) {
+
+    }
+}
